aa89ec65fd2806639a522b3dfebe8759152ad62a
Fərid Sevindikov <you@example.com>
2021-06-13 16:35:00 +0400

    Calculator

---
+++ main.js
@@ -0,0 +1,9 @@
+function Func(deyir){
+    calculator.anwser.value=calculator.anwser.value+deyir;
+}
+function an(){
+    calculator.anwser.value=eval(calculator.anwser.value)
+}
+function delet(){
+    calculator.anwser.value="";
+} 
